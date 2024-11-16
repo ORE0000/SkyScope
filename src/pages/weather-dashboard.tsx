4df62e44e7ed7a0d0e22d3b0ed3370797516a6f3
ORE0000 <ashutoshpant.855@gmail.com>
2024-11-16 11:20:56 +0530

    Update project code with new features/fixes

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import { RefreshCw } from 'lucide-react'
 
 const WeatherDashboard = () => {
-  return (
-    <div>WeatherDashboard</div>
-  )
-}
+  return(
+    <div className='space-y-4'>
+    {/* Favorite cities */}
+    <div className='flex items-center justify-between'>
+      <h1 className='text-xl font-bold tracking-tight'>My Location</h1>
+      <Button variant={'outline'}
+      size={"icon"}
+      // onClick={handelRefresh}
+      // disabled={}
+      >
+        <RefreshCw className='h-4 w-4'/>
+      </Button>
+    </div>
+    {/* Current and Hourly weather */}
+  </div>
+  );
+};
 
 export default WeatherDashboard
